09c9fb75a0be102d507694c8dbd7085365c333b1
Obed Loga <72562778+LogaObed@users.noreply.github.com>
2024-03-21 18:40:19 -0600

    Actualizar enviarcorreo.php

--- database/enviarcorreo.php
+++ database/enviarcorreo.php
@@ -18,7 +18,7 @@ function enviarCorreo($correo, $nombre, $asunto, $html)
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'obedloga97@gmail.com';                     //SMTP username
-        $mail->Password   = 'bxutmkkrfccszxsn';                               //SMTP password
+        $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         //Recipients
